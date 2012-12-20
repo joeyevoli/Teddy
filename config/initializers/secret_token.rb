538c7c7f10c52ab78f19fc42fbba286d2431e8a6
Joe Yevoli <joe.yevoli@gmail.com>
2012-12-20 08:56:54 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teddy::Application.config.secret_token = '93f99a19cb86befee63c8a319785b9dd237232f3770d38c63ff4e371f9a68bdd116d77c823c478b322d03c06ad1016d00e39a20e59a94b57a90fed30d8a32da3'
